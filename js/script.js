c41582191d37306881f9c6b666f7642e023318a7
marcopriests <prtmrc98@gmail.com>
2025-03-20 13:42:49 +0100

    [UPD] Mail - variabile mailUtente creata

--- js/script.js
+++ js/script.js
@@ -3,4 +3,9 @@
 //creo un array con le email degli invitati alla festa
 const invitati = ["reverendo-green@mail.com", "colonnello-mustard@mail.com", "professor-plum@mail.com", "miss-scarlett@mail.com", "dottoressa-orchid@mail.com", "contessa-peacock@mail.com"]
 
-console.log(invitati)
+console.log("le email degli invitati sono: " + invitati)
+
+
+const mailUtente = prompt("Inserisci la tua mail")
+
+console.log("La mail inserita dall'utente è: " + mailUtente)
