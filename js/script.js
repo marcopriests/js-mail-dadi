b9b05c38bdaee0ef919dad82d204fa188d5eac47
marcopriests <prtmrc98@gmail.com>
2025-03-20 13:41:23 +0100

    [UPD] Mail - array invitati creato

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Mail
+
+//creo un array con le email degli invitati alla festa
+const invitati = ["reverendo-green@mail.com", "colonnello-mustard@mail.com", "professor-plum@mail.com", "miss-scarlett@mail.com", "dottoressa-orchid@mail.com", "contessa-peacock@mail.com"]
+
+console.log(invitati)
